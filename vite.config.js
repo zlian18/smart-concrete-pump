9231ee9e95cf28c531be3a1f271dac4f3a2fc64d
Zhan Lian <zlian2000@outlook.com>
2025-02-09 01:42:52 +1000

    add missing ./

--- vite.config.js
+++ vite.config.js
@@ -11,12 +11,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: 'index.html',
-        slabs: 'slabs.html',
-        block: 'block.html',
-        driveway: 'driveway.html',
-        areas: 'areas.html',
-        contacts: 'contacts.html',
+        index: './index.html',
+        slabs: './slabs.html',
+        block: './block.html',
+        driveway: './driveway.html',
+        areas: './areas.html',
+        contacts: './contacts.html',
       }
     },
     outDir: 'dist',  // Default output folder
