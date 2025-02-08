5cc4d8f507ddf63aa1a068d7de2930569cc9b320
Zhan Lian <zlian2000@outlook.com>
2025-02-09 01:38:10 +1000

    add multi pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import multiPages from 'vite-plugin-multi-pages'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    multiPages(),
   ],
-})
+  base: './',  // Ensures correct relative paths for assets
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        slabs: 'slabs.html',
+        block: 'block.html',
+        driveway: 'driveway.html',
+        areas: 'areas.html',
+        contacts: 'contacts.html',
+      }
+    },
+    outDir: 'dist',  // Default output folder
+    }
+  })
